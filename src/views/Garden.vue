af0d31fca4ca2e0d03f3bb3cb205e0465cbc338f
Meena <msomasundaram@dotdash.com>
2021-03-04 16:02:47 -0500

    added elements to garden.vue

--- src/views/Garden.vue
+++ src/views/Garden.vue
@@ -1,5 +1,240 @@
 <template>
-    
+    <div class ="garden">
+       <v-container class ="my-4" >
+           <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>     
+         </v-row>
+          <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>     
+         </v-row>
+        </v-container>   
+         <v-container class ="my-3" >
+           <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>     
+         </v-row>
+          <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>      
+         </v-row>
+         <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>      
+         </v-row>
+        </v-container> 
+        <v-container class ="my-8" >
+           <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>    
+         </v-row>
+          <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>      
+         </v-row>
+         <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>      
+         </v-row>
+          <v-row>
+               <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col> 
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>
+                <v-col>
+                   <v-card> </v-card>
+                </v-col>    
+         </v-row>
+        </v-container> 
+    </div>    
 </template>
 
 <script>
